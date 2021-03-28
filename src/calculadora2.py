ef6ec7b90e8262489456247d8943a7242bd7a24a
herykeven <heryykeven@gmail.com>
2021-03-28 15:08:20 -0300

    my first project

---
+++ src/calculadora2.py
@@ -0,0 +1,33 @@
+#Complete as funcoes a seguir
+
+def soma(a, b):
+	print ("Soma é: ", a+b)
+
+def subtrai(a, b):
+	print ("Subtração é: ", a-b)
+
+def multiplica(a, b):
+	print ("Multiplicação é: ", a*b)
+
+def divide(a, b):
+        if (b == 0):
+            print("Não pode dividir por zero!")
+        else:
+            print("O resultado da divisão é: ", a / b)
+	
+
+
+#Programa principal
+	
+
+print("Calculadora simples")
+
+num1 = float(input("Insira o primeiro numero: "))
+num2 = float(input("Insira o segundo numero: "))
+
+soma(num1, num2)
+subtrai(num1, num2)
+multiplica(num1, num2)
+divide(num1, num2)
+
+print(":)")
